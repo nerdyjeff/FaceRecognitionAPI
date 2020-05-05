e7ed69130c3cf1d9b8e53c7e39b21cc68cb61c13
admin <Office@nerdyjeff.local>
2020-05-05 15:40:29 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+ const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req, res) => {
+    res.send(database.users);
+});
+
+//and update the login table in the database
+app.post('/signin', (req,res) => { signin.handleSignin(req,res,db,bcrypt)})
+
+// Calling the function register.js to get a new user and store it in database
+//And injecting the dependencies the function needs to run
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+//Route to get/return a user's profile
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req, res, db)})
+
+//Updating the user entry and increase their count
+app.post('/imageurl',(req,res) => { image.handleApiCall(req,res,db)})
+
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('server is running');
+})
